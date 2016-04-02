09ee2b63246299cec45be07207ac371ee8789e0d
Visnu Pitiyanuvath <visnup@gmail.com>
2016-04-02 00:09:48 -0700

    fix flashing and height issues
    by moving font loading to linked stylesheet

--- src/name.jsx
+++ src/name.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { flatten, groupBy, isEmpty, map, omit, sumBy } from 'lodash'
+import { groupBy, isEmpty, map, omit } from 'lodash'
 
 import css from './name.css'
 import Horizon from './horizon'
