090501f8693a8c7452ef421b39b9939a67f3cbaf
Visnu Pitiyanuvath <visnup@gmail.com>
2019-04-27 13:18:10 -0700

    serialize errors

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const { json } = require("micro");
 const cors = require("micro-cors")();
+const serializeError = require("serialize-error");
 const { BigQuery } = require("@google-cloud/bigquery");
 
 const credentials = JSON.parse(
   process.env.GOOGLE_APPLICATION_CREDENTIALS || "{}"
 );
-delete process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const client = new BigQuery({ credentials, projectId: credentials.project_id });
 
 module.exports = cors(async req => {
-  if (req.method !== "POST") return {};
-
-  const { query, params, location = "US" } = await json(req);
-
-  const client = new BigQuery({
-    credentials,
-    projectId: credentials.project_id
-  });
-  const [rows] = await client.query({ query, params, location });
-  return rows;
+  try {
+    if (req.method !== "POST") return {};
+    const { query, params, location = "US" } = await json(req);
+    const [rows] = await client.query({ query, params, location });
+    return rows;
+  } catch (error) {
+    return serializeError(error);
+  }
 });
